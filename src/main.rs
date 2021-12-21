aed619f4beaa787a0e581ae32e965a1807a3fb09
Mattia Procopio <promat85@gmail.com>
2021-12-21 12:42:56 +0100

    Finish off

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use std::{thread, time};
+use std::{process, thread, time};
 use sysinfo::{System, SystemExt};
+use structopt::StructOpt;
+use minreq::{post, Error};
 
 static INTERVAL: time::Duration = time::Duration::from_secs(15);
+static HOST: &'static str = "http://astromatto.com:8000/hook";
 
-fn notify_via_telegram() {
-    println!("notifying");
+#[derive(StructOpt)]
+struct CliArgs {
+    /// The pattern to look for
+    api_token: String,
 }
 
-fn main() {
+fn notify_via_telegram(token: &String) -> Result<(), Error> {
+    let _response = post(format!("{}/{}", &HOST, &token)).send()?;
+    process::exit(0);
+}
+
+fn main() -> Result<(), Error> {
+    let args = CliArgs::from_args();
+    let api_token = args.api_token;
+
     loop {
 	let all_procs = System::new_all();
-	match all_procs.process_by_name("kstars")[..] {
-	    [] => notify_via_telegram(),
-	    [..] => (),
+	let kstars_proc = all_procs.process_by_name("kstars");
+	if kstars_proc.is_empty() {
+	    let _resp = notify_via_telegram(&api_token)?;
 	};
 	thread::sleep(INTERVAL);
     }
