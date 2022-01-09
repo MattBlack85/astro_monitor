0897af9236087f5203bca13f9f9a37737655a3c0
Mattia Procopio <promat85@gmail.com>
2022-01-09 14:55:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use minreq::{post, Error};
+use std::process::{Command, Stdio};
 use std::{process, thread, time};
 use structopt::StructOpt;
 use sysinfo::{System, SystemExt};
-use std::process::Command;
 
 static INTERVAL: time::Duration = time::Duration::from_secs(15);
 static HOST: &'static str = "http://astromatto.com:11111/hook";
@@ -16,9 +16,9 @@ struct CliArgs {
 
 fn notify_via_telegram(token: &String) -> Result<(), Error> {
     let response = post(format!("{}/{}", &HOST, &token)).send()?;
-    
+
     println!("Kstars stopped running, sending a notification");
-    
+
     process::exit(match response.status_code {
         200 => {
             println!("Notification sent! Bye!");
@@ -32,16 +32,59 @@ fn notify_via_telegram(token: &String) -> Result<(), Error> {
 }
 
 fn check_lsof_on_system() -> u8 {
-    let result = Command::new("which")
-        .arg("lsof")
-        .output()
-	.expect("error");
-    let output = String::from_utf8(result.stdout).expect("fail to parse output").to_string();
+    let result = Command::new("which").arg("lsof").output().expect("error");
+    let output = String::from_utf8(result.stdout)
+        .expect("fail to parse output")
+        .to_string();
     match output.as_ref() {
-	"" => return 0,
-	_ => return 1,
+        "" => return 0,
+        _ => return 1,
     };
-	
+}
+
+fn fd_report() -> String {
+    let lsof = Command::new("lsof")
+        .arg("/")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed echo command");
+
+    let awk = Command::new("awk")
+        .arg("{print $1}")
+        .stdin(lsof.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("AWK error");
+
+    let sort = Command::new("sort")
+        .stdin(awk.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Sort failed");
+
+    let uniq = Command::new("uniq")
+        .arg("-c")
+        .stdin(sort.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Uniq failed");
+
+    let sort_again = Command::new("sort")
+        .args(["-r", "-n"])
+        .stdin(uniq.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Fail sort again");
+
+    let head = Command::new("head")
+        .args(["-n", "20"])
+        .stdin(sort_again.stdout.unwrap())
+        .output()
+        .expect("Head failed");
+
+    let report = String::from_utf8_lossy(&head.stdout);
+
+    return report.to_string();
 }
 
 fn main() -> Result<(), Error> {
@@ -51,27 +94,34 @@ fn main() -> Result<(), Error> {
     let mut found: bool = false;
 
     if fd_monitor {
-	let res = check_lsof_on_system();
-	if res == 0 {
-	    println!("`--fd-monitor flag` passed but `lsof` command not found or not available in PATH, aborting!");
-	    process::exit(0);
-	}
+        let res = check_lsof_on_system();
+        if res == 0 {
+            println!("`--fd-monitor` flag passed but `lsof` command not found or not available in PATH, aborting!");
+            process::exit(0);
+        }
     }
 
     println!("Looking for Kstars!");
-    
-    // Find a way to run lsof / | awk '{print $1}' | sort | uniq -c | sort -r -n | head -n 20 periodically
+
     loop {
         let all_procs = System::new_all();
         let kstars_proc = all_procs.process_by_name("kstars");
         if kstars_proc.is_empty() {
             let _resp = notify_via_telegram(&api_token)?;
         };
-	match found {
-	    true => (),
-	    false => println!("Kstars found! Started monitoring"),
-	};
-	found = true;
+        match found {
+            true => (),
+            false => println!("Kstars found! Started monitoring"),
+        };
+        found = true;
+
+        let fd_result = match fd_monitor {
+            true => fd_report(),
+            false => "".to_string(),
+        };
+
+        println!("{}", fd_result);
+
         thread::sleep(INTERVAL);
     }
 }
