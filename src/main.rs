a1ca0c4c92e1a7de24d4fcf1815e03049193eb75
Mattia Procopio <promat85@gmail.com>
2023-01-18 15:22:53 +0100

    Add backup functionality (#13)
    * Add additional cli arg for Kstars
    
    * Rework and simplify telegram notification fn
    
    * Move generic things to lib.rs
    
    * Implement backup for Kstars DB and indi device configs

--- src/main.rs
+++ src/main.rs
@@ -4,68 +4,35 @@ extern crate minreq;
 extern crate structopt;
 extern crate sysinfo;
 
+use astromonitor::{CliArgs, Paths, HOST, INTERVAL};
 use chrono::SecondsFormat;
-use minreq::{post, Error};
 use std::process::{Command, Stdio};
-use std::{process, thread, time};
+use std::{process, thread};
 use structopt::StructOpt;
 use sysinfo::{ProcessExt, System, SystemExt};
+
+mod backup;
 mod checks;
 mod monitoring;
 
-static INTERVAL: time::Duration = time::Duration::from_secs(15);
-static HOST: &'static str = "http://astromatto.com:11111/hook";
-
-#[derive(StructOpt)]
-struct CliArgs {
-    api_token: String,
-    #[structopt(long)]
-    fd_monitor: bool,
-    #[structopt(long)]
-    system_monitor: bool,
-}
-
-struct Paths {
-    folder_path: String,
-    logs_path: String,
-    home_path: String,
-}
-
-impl Paths {
-    fn logs_full_path(&self) -> String {
-        let logs_full_path = format!("{}/{}", self.root_path(), self.logs_path);
-        return logs_full_path;
-    }
-
-    fn root_path(&self) -> String {
-        let root_path = format!("{}/{}", self.home_path, self.folder_path);
-        return root_path;
-    }
-
-    fn init() -> Self {
-        Self {
-            folder_path: String::from(".local/share/astromonitor"),
-            logs_path: String::from("logs"),
-            home_path: dirs::home_dir().unwrap().as_path().display().to_string(),
+fn notify_via_telegram(token: &String) {
+    match minreq::post(format!("/hook/{}/{}", &HOST, &token)).send() {
+        Ok(r) => {
+            process::exit(match r.status_code {
+                200 => {
+                    println!("Notification sent! Bye!");
+                    0
+                }
+                _ => {
+                    println!("Notification failed with status: {}", r.status_code);
+                    1
+                }
+            });
         }
-    }
-}
-
-fn notify_via_telegram(token: &String) -> Result<(), Error> {
-    let response = post(format!("{}/{}", &HOST, &token)).send()?;
-
-    println!("Kstars stopped running, sending a notification");
-
-    process::exit(match response.status_code {
-        200 => {
-            println!("Notification sent! Bye!");
-            0
+        Err(e) => {
+            println!("The request couldn't be sent to the bot: {}", e);
         }
-        _ => {
-            println!("Notification failed with status: {}", response.status_code);
-            1
-        }
-    });
+    };
 }
 
 /// Runs lsof on the whole filesystem, sorting results, dropping duplicates and returns
@@ -111,20 +78,29 @@ fn fd_report() {
     println!("{}", report);
 }
 
-fn main() -> Result<(), Error> {
+fn main() {
     let args = CliArgs::from_args();
     let api_token = args.api_token;
     let fd_monitor = args.fd_monitor;
+    let do_backup = args.do_backup;
+    let kstars = args.kstars;
     let paths = Paths::init();
 
     // Boostrap the main folder where logs and our things will be stored
     match checks::vault::build_astromonitor_folder_tree() {
         Ok(()) => (),
-        Err(e) => panic!(
+        Err(e) => println!(
             "The folder to store logs cannot be created, reason => {}",
             e
         ),
     };
+
+    if do_backup {
+        match backup::database::send_db(&paths, &api_token) {
+            Ok(_) => (),
+            Err(s) => println!("{}", format!("Error while trying to make a backup: {}", s)),
+        }
+    }
 
     if fd_monitor {
         match checks::system::lsof_on_system() {
@@ -136,40 +112,43 @@ fn main() -> Result<(), Error> {
         };
     }
 
-    println!("Looking for Kstars!");
+    if kstars {
+        println!("Looking for Kstars!");
 
-    let mut system = System::new();
-    let start_time: String = chrono::Local::now().to_rfc3339_opts(SecondsFormat::Secs, false);
+        let mut system = System::new();
+        let start_time: String = chrono::Local::now().to_rfc3339_opts(SecondsFormat::Secs, false);
 
-    // Check if Kstars is already running when astromonitor starts and store the pid
-    // if that's the case, otherwise exit gracefully.
-    system.refresh_processes();
-    system.process_by_exact_name("kstars");
-    let kstars = system.process_by_exact_name("kstars");
-    if kstars.is_empty() {
-        println!("It seems you haven't started Kstars yet, please do it first.");
-        process::exit(0);
-    } else {
-        let _pid: i32 = kstars[0].pid();
-    };
-
-    loop {
+        // Check if Kstars is already running when astromonitor starts and store the pid
+        // if that's the case, otherwise exit gracefully.
         system.refresh_processes();
-        let kstars_proc = system.process_by_exact_name("kstars");
-        if kstars_proc.is_empty() {
-            let _resp = notify_via_telegram(&api_token)?;
+        system.process_by_exact_name("kstars");
+        let kstars = system.process_by_exact_name("kstars");
+        if kstars.is_empty() {
+            println!("It seems you haven't started Kstars yet, please do it first.");
+            process::exit(0);
+        } else {
+            let _pid: i32 = kstars[0].pid();
         };
 
-        match fd_monitor {
-            true => fd_report(),
-            false => (),
-        };
+        loop {
+            system.refresh_processes();
+            let kstars_proc = system.process_by_exact_name("kstars");
+            if kstars_proc.is_empty() {
+                println!("Kstars stopped running, sending a notification");
+                notify_via_telegram(&api_token);
+            };
 
-        match args.system_monitor {
-            true => monitoring::resources::cpu_and_memory(&start_time, &paths.logs_full_path()),
-            false => (),
+            match fd_monitor {
+                true => fd_report(),
+                false => (),
+            };
+
+            match args.system_monitor {
+                true => monitoring::resources::cpu_and_memory(&start_time, &paths.logs_full_path()),
+                false => (),
+            }
+
+            thread::sleep(INTERVAL);
         }
-
-        thread::sleep(INTERVAL);
-    }
+    };
 }
